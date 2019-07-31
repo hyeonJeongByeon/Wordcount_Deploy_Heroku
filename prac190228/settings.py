bba80985be9f497319b61bccca3a148fd7c2f779
hyeonJeongByeon <jungjung1218@gmail.com>
2019-07-30 21:45:10 -0400

    last comment

--- prac190228/settings.py
+++ prac190228/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','$j=5_utt@0h1%&kdr$@po_nkc^lra^c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool (os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
